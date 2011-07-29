2ab9ab7d5ef9ad4c0f0f41a1dcec6f4fedf3127b
Brian Tanaka <btanaka@apple.com>
2011-07-28 20:07:07 -0700

    wired up chapter display from markdown

--- gitology.rb
+++ gitology.rb
@@ -1,9 +1,58 @@
 require 'rubygems'
 require 'sinatra';
 
-TITLE = "Gitology - A wealth of knowledge about Git"
 
+
+#
+# constants
+#
+TITLE = "Gitology - A wealth of knowledge about Git"
+BASEPATH = "." # base filesystem path
+CHAPTERDIR = "chapters" # where the chapters live 
+CHAPTEREXT = "mdwn"
+
+
+#
+# classy
+#
+class Gitology
+
+  def initialize()
+    @foo = "bar"
+  end
+
+  def foo
+    # getter for foo
+    return @foo
+  end
+
+  def chapter_to_string(chapter_name)
+    @pathname = CHAPTERDIR + "/" + chapter_name + "." + CHAPTEREXT
+    @chapter_contents = File.open("#{@pathname}", 'r') { |file| file.read }
+    return @chapter_contents
+  end
+
+end
+
+gitology = Gitology.new
+
+
+
+#
+# default route at slash
+#
 get '/' do
   haml :index
 end
 
+
+
+#
+# display a chapter
+#
+get '/chapter/:argument' do
+  @requested_chapter = params[:argument]
+  @content = gitology.chapter_to_string(@requested_chapter)
+  # TODO: call object method here
+  haml :chapter
+end
